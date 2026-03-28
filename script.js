5fa450733f1f2b4c975225833f8073e3a899463f
MuhammadUsman-t77 <khakiusman00@gmail.com>
2026-03-28 19:37:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+
+let string=''; 
+let buttons=document.querySelectorAll('.button');
+     Array.from(buttons).forEach((button)=>{
+        button.addEventListener('click',(e)=>{
+
+
+
+            if(e.target.innerHTML== '='){
+                string=eval(string);
+                document.querySelector('input').value=string;
+
+
+
+            }else if(e.target.innerHTML=='C'){
+                string=''
+                document.querySelector('input').value=string;
+            }
+
+            //    else if(e.target.innerhtml=="D"){
+              // string=string.length-1
+
+               //}
+                else{
+               // console.log(e.target)
+                string=string + e.target.innerHTML;
+                document.querySelector('input').value=string; 
+            }
+        })
+    })
+
+
+
